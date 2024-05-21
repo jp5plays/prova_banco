43fc5644225c7e4f7a20c6ba1ade651fd3c0b066
jp5plays <143205370+jp5plays@users.noreply.github.com>
2024-05-21 17:22:42 -0300

    Add files via upload

---
+++ lista-01.sql
@@ -0,0 +1,2 @@
+insert into Livros (titulo,autor,ano_publicacao,disponivel,categoria,isbn,editora,quantidade_paginas,idioma) values
+("As Crônicas de Nárnia","C.S. Lewis",1950,true,"Fantasia","978-0064471190","HaperCollins",768,"Inglê");
